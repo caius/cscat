bc6aae858386da0cf374590c7eb387e46688629d
Caius Durling <dev@caius.name>
2019-10-29 23:55:31 +0000

    cscat setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cscat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
