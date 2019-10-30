f9b0b3d1651a5870d165f5ef5b3bb9c30f032569
Caius Durling <dev@caius.name>
2019-10-30 00:35:17 +0000

    Read from STDIN on - too

--- Program.cs
+++ Program.cs
@@ -3,31 +3,40 @@ using System.IO;
 
 namespace cscat
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            if (args.Length == 0)
-            {
-                Console.WriteLine("Error: no args");
-                return;
-            }
+      if (args.Length == 0)
+      {
+        Console.WriteLine("Error: no args");
+        return;
+      }
 
-            foreach (var path in args)
-            {
-                try
-                {
-                    using (StreamReader sr = new StreamReader(path))
-                    {
-                        String line = sr.ReadToEnd();
-                        Console.WriteLine(line);
-                    }
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("cat: {0:G}: No such file or directory", path);
-                }
-            }
+      foreach (var path in args)
+      {
+        if (path == "-")
+        {
+          Console.WriteLine("Reading stdin");
+          Stream input = Console.OpenStandardInput();
+          input.CopyTo(Console.OpenStandardOutput());
         }
+        else
+        {
+          try
+          {
+            using (StreamReader sr = new StreamReader(path))
+            {
+              String line = sr.ReadToEnd();
+              Console.WriteLine(line);
+            }
+          }
+          catch (Exception)
+          {
+            Console.WriteLine("cat: {0:G}: No such file or directory", path);
+          }
+        }
+      }
     }
+  }
 }
