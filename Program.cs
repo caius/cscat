b0540c97de19ce60d195c72b1a83c612322126b2
Caius Durling <dev@caius.name>
2021-08-21 12:51:51 +0100

    Basic formatted file

--- Program.cs
+++ Program.cs
@@ -2,16 +2,11 @@ using System;
 
 namespace cscat
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-          // Run through the arguments and check
-          foreach (string a in args) {
-            Console.WriteLine(a);
-          }
 
-          Console.WriteLine("Hello World!");
-        }
     }
+  }
 }
